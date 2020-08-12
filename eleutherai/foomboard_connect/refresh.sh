e884531d4487490f1acf034ca5b69ab6f996f3e3
Kevin Watkins <kevin.watkins@gmail.com>
2020-08-12 11:25:48 -0400

    foomboard_connect nbconvert longer timeout

--- eleutherai/foomboard_connect/refresh.sh
+++ eleutherai/foomboard_connect/refresh.sh
@@ -9,5 +9,5 @@ set -e -x
 
 cd "$outdir"
 python "$scriptdir"/fetch_notebook.py "$notebookurl" "$notebook"
-jupyter nbconvert --no-input --to notebook --output out --execute "$notebook"
+jupyter nbconvert --no-input --to notebook --output out --ExecutePreprocessor.timeout=240 --execute "$notebook"
 python "$scriptdir"/extract_images.py "$scriptdir" .
