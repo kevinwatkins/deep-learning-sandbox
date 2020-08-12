f1f64b923fb89844948e8d7041f0ffaed3289a18
Kevin Watkins <kevin.watkins@gmail.com>
2020-08-12 14:20:36 -0400

    extract second plot

--- eleutherai/foomboard_connect/extract_images.py
+++ eleutherai/foomboard_connect/extract_images.py
@@ -6,22 +6,22 @@ outdir = sys.argv[2]
 with open('out.ipynb', 'r') as f:
     j = json.load(f)
 
-outs = [o['data']
+outs = {c['metadata']['id']: o['data']
     for c in j['cells']
     if c['cell_type'] == 'code'
     for o in c['outputs']
     if o['output_type'] == 'display_data'
     if 'image/png' in o['data']
       or 'text/markdown' in o['data']
-]
+}
 
 manifest = []
 
-def save_output_cell(name, index, dtype, wrapper=None):
-    if index >= len(outs) or not dtype in outs[index]:
+def save_output_cell(cell_id, name, dtype, wrapper=None):
+    if cell_id not in outs or dtype not in outs[cell_id]:
         return
     
-    data = outs[index][dtype]
+    data = outs[cell_id][dtype]
     mode = 'w'
     if dtype == 'text/markdown':
         if isinstance(data, list):
@@ -40,8 +40,10 @@ def save_output_cell(name, index, dtype, wrapper=None):
     with open(os.path.join(outdir, name), mode) as f:
         f.write(data)
 
-save_output_cell('table.html', 0, 'text/markdown', wrapper=os.path.join(scriptdir, 'table_wrapper.html'))
-save_output_cell('plot.png', 1, 'image/png')
+save_output_cell('dqLt9-eVKeS9', 'table.html', 'text/markdown', wrapper=os.path.join(scriptdir, 'table_wrapper.html'))
+save_output_cell('P920JQLa4hht', 'plot.png', 'image/png')
+save_output_cell('P920JQLa4hht', 'compute_plot.png', 'image/png')
+save_output_cell('ZkPlx9xxiaPV', 'tokens_plot.png', 'image/png')
 
 with open(os.path.join(outdir, 'MANIFEST'), 'w') as f:
     f.writelines(manifest)
